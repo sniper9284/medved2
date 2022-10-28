05e1b7b05353a8969d8c0407fb8ad2ec20f6aafc
Sniper <sniper-sochi@yandex.ru>
2022-10-28 10:53:19 +0300

    Initial commit

---
+++ src/main/java/com/example/medved2/Medved2Application.java
@@ -0,0 +1,13 @@
+package com.example.medved2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medved2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medved2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medved2/Medved2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medved2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medved2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
